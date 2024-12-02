8f55544e4bd42bcad7d152b776fb096b519fe304
Jimmy Conner <jimmy@sqmail.org>
2024-12-02 15:03:24 -0600

    Spell vendor correctly

--- pre_commit_hooks/helpers/locate.sh
+++ pre_commit_hooks/helpers/locate.sh
@@ -9,7 +9,7 @@ exec_command=""
 
 # A phar file will need to be called by php
 prefixed_local_command="php $local_command"
-vender_src_command="src/$vender_command"
+vendor_src_command="src/$vendor_command"
 
 if [ -f "$vendor_command" ]; then
     exec_command=$vendor_command
@@ -17,14 +17,14 @@ elif hash $global_command 2>/dev/null; then
     exec_command=$global_command
 elif [ -f "$local_command" ]; then
     exec_command=$prefixed_local_command
-elif [ -f "$vender_src_command" ]; then
-    exec_command=$vender_src_command
+elif [ -f "$vendor_src_command" ]; then
+    exec_command=$vendor_src_command
 else
     echo -e "${bldred}No valid ${title} found!${txtrst}"
     echo "Please have one available as one of the following:"
     echo " * $local_command"
     echo " * $vendor_command"
-    echo " * $vender_src_command"
+    echo " * $vendor_src_command"
     echo " * $global_command"
     exit 1
 fi

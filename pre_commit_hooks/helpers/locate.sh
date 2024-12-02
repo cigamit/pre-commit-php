f9ede03b35dc730032669c98206e34df37c2c3dd
Jimmy Conner <jimmy@sqmail.org>
2024-12-02 14:58:04 -0600

    Check for src in front of local command

--- pre_commit_hooks/helpers/locate.sh
+++ pre_commit_hooks/helpers/locate.sh
@@ -9,17 +9,22 @@ exec_command=""
 
 # A phar file will need to be called by php
 prefixed_local_command="php $local_command"
+local_src_command="src/$local_command"
+prefixed_local_src_command="php $local_src_command"
 
 if [ -f "$vendor_command" ]; then
     exec_command=$vendor_command
 elif hash $global_command 2>/dev/null; then
     exec_command=$global_command
 elif [ -f "$local_command" ]; then
+    exec_command=$prefixed_local_src_command
+elif [ -f "$local_src_command" ]; then
     exec_command=$prefixed_local_command
 else
     echo -e "${bldred}No valid ${title} found!${txtrst}"
     echo "Please have one available as one of the following:"
     echo " * $local_command"
+    echo " * $local_src_command"
     echo " * $vendor_command"
     echo " * $global_command"
     exit 1
